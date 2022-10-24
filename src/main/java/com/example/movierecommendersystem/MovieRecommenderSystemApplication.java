c1387e2e8fd0c4e6ab210319667aad1ea975d73a
edkehayova18 <edkehayova18@codingburgas.bg>
2022-10-24 13:52:40 +0300

    Add lesson1

--- src/main/java/com/example/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ src/main/java/com/example/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -1,13 +1,18 @@
 package com.example.movierecommendersystem;
 
+import lesson1.RecommenderImplementation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+        RecommenderImplementation recommender = new RecommenderImplementation();
+        String[] result = recommender.recommendMovies("finding Dory");
+        System.out.println(Arrays.toString(result));
     }
 
 }
---
+++ src/main/java/lesson1/ContentBasedFilter.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class ContentBasedFilter {
+    public String[]getRecommendations(String movie){
+        return new String[] {"Happy feet", "Ice age", "Shark Tale"};
+    }
+
+}
